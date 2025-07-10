0d036ce071aede34e2495e0d716af91f3e3fdc37
yourfather1005 <zhayribilal@gmail.com>
2025-07-10 22:19:48 +0200

    add

---
+++ model/db.php
@@ -0,0 +1,36 @@
+﻿<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
+class Database {
+    private $connection;
+
+    public function __construct() {
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASS'];
+
+        try {
+            $this->connection = new PDO(
+                "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+                $user,
+                $password
+            );
+            
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("errore: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+
+    
+    public function close() {
+        $this->connection = null;
+    }
+}
