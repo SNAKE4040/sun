9710dfa284ffd2f14ce86d08fdc15ce08ebc1af4
yourfather1005 <zhayribilal@gmail.com>
2025-07-10 12:51:10 +0200

    modification

---
+++ view/log.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+   
+    .a{
+        margin: 0 ;
+        padding: 20px;
+        box-sizing: border-box;
+        font-family: "poppins", sans-serif;
+        position: relative;
+        width: 300px;
+        height: 420px;
+        background: #030013;
+        border-radius: 8px;
+        overflow: hidden;
+
+    }
+    body{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        background-color: #FfFFFf;/*#ffc9be;*/
+    }
+    .a::before{
+        content: "";
+        position: absolute;
+        width: 300px;
+        height: 420px;
+        background: linear-gradient(0deg, transparent, #fe03f5, #fe03f6);
+        top: -50%;
+        left: -50%;
+        transform-origin: bottom right;
+        animation : anime 7s linear infinite;
+    }
+    .a::after{
+        content: "";
+        position: absolute;
+        width: 300px;
+        height: 460px;
+        background: linear-gradient(0deg, transparent, #26fdfa, #1bfd00);
+        top: -50%;
+        left: -50%;
+        transform-origin: bottom right;
+        animation : anime 7s linear infinite;
+        animation-delay: 3s;
+    }
+    @keyframes anime{
+        100%{
+            transform: rotate(360deg);
+        }
+    }
+    .b{
+        position : absolute;
+        inset: 3px;
+        background:#F0F0FF;/* #fff8be;*/
+        border-radius: 8px;
+        z-index: 10;
+        padding: 30px 40px;
+        display: flex;
+        flex-direction: column;
+
+        
+    }
+    .b h2{
+        color: #000;
+        text-align: center;
+        letter-spacing: 3px;
+    }
+    .c{
+        position: relative;
+        width: 300px;
+        margin-top: 40px;
+
+    }
+    .c input{
+        padding: 8px 4px 10px;
+        
+        background: transparent;
+        
+        font-size: 18px;
+        outline: none;
+        color: #000;
+    }
+    input[type="submit"]{
+        /*border: 1px solid #000;*/
+        cursor: pointer;
+        background: #93eeda;
+        border-radius: 10px;
+        padding : 12px 8px 12px 8px;
+        margin-left: 60px;
+        margin-left: 60px;
+        
+    }
+    .c input[type="email"]{
+        border: 1px solid #000;
+       
+    }
+    
+    
+
+    </style>
+</head>
+<body>
+ 
+        <div class="a">
+         <form action="" method="post">
+            <div class="b">
+                <h2>LOG IN</h2>
+                <div class="c">
+                    <input type="text" name="nom" placeholder="full name or email">
+                </div>
+                <div class="c">
+                    <input type="password" name="password" placeholder="password">
+                </div>
+                <div class="c">
+                    <input type="submit" value="Sign Up" name="log in">
+                </div>
+            </div>
+            </form>
+        </div>
+        
+</body>
+</html>
